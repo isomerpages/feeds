41e6c8566177d8c054840608b82ce0603a98bb5a
LoneRifle <LoneRifle@users.noreply.github.com>
2021-01-15 15:01:07 +0800

    Add Feedly widget

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,6 +8,23 @@ fetch(url)
         { title: 'Type', field: 'type', headerFilter: true },
         { title: 'Government Body', field: 'governmentBody', headerFilter: true },
         { title: 'Name', field: 'name' },
+        { 
+          title: 'Feedly', 
+          field: 'feedUrl', 
+          formatter: (cell) => {
+            const url = cell.getValue()
+            const img = document.createElement('img')
+            img.setAttribute('src', 'http://s3.feedly.com/img/follows/feedly-follow-rectangle-flat-small_2x.png')
+            img.setAttribute('width', '61')
+            img.setAttribute('height', '20')
+            img.setAttribute('alt', 'Follow this on Feedly')
+            const a = document.createElement('a')
+            a.appendChild(img)
+            a.setAttribute('href', `https://feedly.com/i/subscription/feed%2F${encodeURIComponent(url)}`)
+            a.setAttribute('target', 'blank')
+            return a
+          },
+        },
         { title: 'Feed URL', field: 'feedUrl' }
       ]
     })
